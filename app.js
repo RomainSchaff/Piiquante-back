2c7e7da52079ba0f138a2ccbaaa8113cd64266f6
Romain Schaff <romainschaff2612@gmail.com>
2023-03-15 20:57:26 +0100

    both origins allowed

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ const app = express();
 app.use(helmet());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://romainschaff.github.io/",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Origin");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
