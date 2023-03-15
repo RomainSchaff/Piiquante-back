3d747f0d466705a41a9db59d89ea3cb76c680abb
Romain Schaff <romainschaff2612@gmail.com>
2023-03-15 19:19:53 +0100

    update app for render

--- app.js
+++ app.js
@@ -27,7 +27,11 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
-  next();
+  if ("OPTIONS" === req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 app.use(express.json());
